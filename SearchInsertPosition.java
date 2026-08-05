5629cd0c890a7a2d0af1194f37fa33fed53a8d8b
Nathani NagaTriveniSahithi <nathaninagatrivenisahithi@gmail.com>
2026-08-05 11:12:51 +0530

    Solved 4 binary search problems

---
+++ SearchInsertPosition.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int first = 0;
+        int last = nums.length - 1;
+        while(first <= last){
+            int mid = first + (last - first) / 2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] < target)
+                first = mid + 1;
+            else
+                last = mid - 1;
+        }
+        for(int i = 0;i < nums.length - 1;i++){
+            if(target > nums[i] && target < nums[i + 1])
+                return i + 1;
+        }
+        if(target < nums[0])
+            return 0;
+
+        return nums.length;
+
+    }
+}
